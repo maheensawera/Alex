c98d1271497de37a69960440d174f50f9da25852
maheensawera <maheensawera1040@gmail.com>
2026-05-10 22:09:01 +0500

    fix typescript error in hook

--- hooks/useInViewAnimation.ts
+++ hooks/useInViewAnimation.ts
@@ -3,9 +3,16 @@
 import { useRef } from "react";
 import { useInView } from "framer-motion";
 
-export function useInViewAnimation(margin = "-100px", once = true) {
-  const ref = useRef<HTMLDivElement>(null);
-  const isInView = useInView(ref, { once, margin });
+// export function useInViewAnimation(margin = "-100px", once = true) {
+//   const ref = useRef<HTMLDivElement>(null);
+//   const isInView = useInView(ref, { once, margin });
 
+//   return { ref, isInView };
+// }
+
+export function useInViewAnimation(margin: any = "-100px", once = true) {
+  const ref = useRef<HTMLDivElement>(null);
+  const isInView = useInView(ref, { once, margin: margin as any });
+  
   return { ref, isInView };
 }
